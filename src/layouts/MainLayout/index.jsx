2259b6fc138b9ca515c0d4011036644c45b9b82c
jishan <jishanpatel.15072003@gmail.com>
2026-08-25 14:08:56 +0530

    Whatsapp number change in Mainlayout file

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -18,7 +18,7 @@ export default function MainLayout() {
 
       {/* WhatsApp Floating Button */}
       <a 
-        href="https://wa.me/919876543210" 
+        href="https://wa.me/919427116700" 
         target="_blank" 
         rel="norenoopener noreferrer"
         className="fixed bottom-6 right-6 z-40 w-14 h-14 bg-[#25D366] text-white rounded-full flex items-center justify-center shadow-[0_0_20px_rgba(37,211,102,0.4)] hover:scale-110 transition-transform duration-300 group"
